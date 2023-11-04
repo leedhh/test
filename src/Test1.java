3f3533430465c98500efa213d10e8f02ac102450
leedh <donghun007@naver.com>
2023-11-04 16:37:43 +0900

    초기 데이터 업로드

--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,6 @@
 public class Test1 {
     public void newA() {
-        System.out.println("테스트테스트 수정1ㅅㄴㅇㄹㄴ아ㅣㄹ아ㅣㄴ라ㅣㅇ나리 다시작업다시작업 다시작업");
+        System.out.println("테스트테스트 수정1ㅅㄴㅇㄹㄴ아ㅣㄹ아ㅣㄴ라ㅣㅇ나리 다시작업다시작업 다시작업 수정ㅈ수정");
     }
 
 
